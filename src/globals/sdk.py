93cf42ae9ccff896a1a9e1a73e673a968e729155
Ice Bear <admin@geode.fi>
2024-05-09 08:07:05 +0200

    Feat: fix circ import issue, sdk already initalizes account.

--- src/globals/sdk.py
+++ src/globals/sdk.py
@@ -6,6 +6,21 @@ from src.globals.exceptions import CouldNotConnect
 from src.globals.env import EXECUTION_API, CONSENSUS_API, PRIVATE_KEY
 
 
+def __set_web3_account(sdk: Geode):
+    """Sets the web3 account to the private key provided in the environment variables."""
+
+    # Create account on Geode's web3py instance
+    acct = sdk.w3.eth.account.from_key(PRIVATE_KEY)
+
+    # Allow Geodefi to use your private key on transact
+    sdk.w3.middleware_onion.add(construct_sign_and_send_raw_middleware(acct))
+
+    # Set default account if one address is used generally
+    sdk.w3.eth.defaultAccount = acct
+
+    return sdk
+
+
 def __init_sdk(exec_api: str, cons_api: str, priv_key: str = None) -> Geode:
     """Initializes the SDK with the provided APIs and private key.
 
@@ -19,16 +34,8 @@ def __init_sdk(exec_api: str, cons_api: str, priv_key: str = None) -> Geode:
     """
     try:
         sdk: Geode = Geode(exec_api=exec_api, cons_api=cons_api)
-
         if priv_key:
-            # Create account on Geode's web3py instance
-            acct = sdk.w3.eth.account.from_key(priv_key)
-
-            # Allow Geodefi to use your private key
-            sdk.w3.middleware_onion.add(construct_sign_and_send_raw_middleware(acct))
-
-            # Set default account if one address is used generally
-            sdk.w3.eth.defaultAccount = acct
+            sdk = __set_web3_account(sdk)
         else:
             raise CouldNotConnect
         return sdk
@@ -38,6 +45,4 @@ def __init_sdk(exec_api: str, cons_api: str, priv_key: str = None) -> Geode:
 
 
 # global SDK
-SDK: Geode = __init_sdk(
-    exec_api=EXECUTION_API, cons_api=CONSENSUS_API, priv_key=PRIVATE_KEY
-)
+SDK: Geode = __init_sdk(exec_api=EXECUTION_API, cons_api=CONSENSUS_API, priv_key=PRIVATE_KEY)
