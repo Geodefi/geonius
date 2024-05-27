203b73d7e700632a22b4380a243b1b4ae5ddf591
Ice Bear <admin@geode.fi>
2024-05-27 14:13:14 +0300

    feat: improve type checking on attribute dict, +chores

--- src/utils/gas.py
+++ src/utils/gas.py
@@ -10,20 +10,20 @@ from src.utils import send_email
 
 
 @http_request
-def fetch_gas() -> tuple:
-    url: str = CONFIG.gas.api
-    return (url, True)
+def __fetch_gas() -> tuple:
+    _url: str = CONFIG.gas.api
+    return (_url, True)
 
 
-def parse_gas(gas) -> tuple[int]:
-    priority: list = CONFIG.gas.parser.priority.split('.')
+def __parse_gas(gas) -> tuple[int]:
+    __priority_fee: list = CONFIG.gas.parser.priority.split('.')
     gas_priority = gas
-    for i in priority:
+    for i in __priority_fee:
         gas_priority = gas_priority[i]
 
-    base_fee: list = CONFIG.gas.parser.fee.split('.')
+    __base_fee: list = CONFIG.gas.parser.base.split('.')
     gas_base_fee = gas
-    for j in base_fee:
+    for j in __base_fee:
         gas_base_fee = gas_base_fee[j]
 
     return float(gas_priority), float(gas_base_fee)
@@ -33,12 +33,13 @@ def get_gas() -> tuple[int]:
     gas: AttributeDict = CONFIG.gas
     if gas:
         if gas.api and gas.parser and gas.max_priority and gas.max_fee:
-            priority_fee, base_fee = parse_gas(fetch_gas())
+            priority_fee, base_fee = __parse_gas(__fetch_gas())
             if priority_fee < gas.max_priority and base_fee < gas.max_fee:
                 return priority_fee, base_fee
             else:
                 log.critical(
-                    f"Undesired GAS price => priority:{priority_fee}, fee:{base_fee}. Will reconsider next time."
+                    f"Undesired GAS price => priority:{priority_fee}, fee:{base_fee}. \
+                        Tx will not be submitted."
                 )
                 send_email(
                     "High Gas Alert",
@@ -46,4 +47,5 @@ def get_gas() -> tuple[int]:
                     gas prices have surpassed the default max settings. Please fix.",
                     [("<file_path>", "<file_name>.log")],
                 )
+                # TODO: raise Exception and catch it in daemon so it doesn't create a tx!
     return (None, None)
