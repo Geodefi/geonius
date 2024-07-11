65b2889a0ae259ef2f53e2a8d97bfd8ea2e60e0b
Ice Bear <admin@geode.fi>
2024-07-11 13:09:03 +0300

    fix: dont_notify_geode. Also add message for dont_notify_geode.

--- src/globals/config.py
+++ src/globals/config.py
@@ -63,7 +63,7 @@ def apply_flags(config: AttributeDict):
     if hasattr(flags, 'ethdo_account') and flags.ethdo_account:
         config.ethdo.account = flags.ethdo_account
     if hasattr(flags, 'dont_notify_geode') and flags.dont_notify_geode is False:
-        config.email.dont_notify_geode = False
+        config.email.dont_notify_geode = True
 
     # put the gas api key in the configuration from the environment variables
     if "<GAS_API_KEY>" in config.gas.api and get_env().GAS_API_KEY:
